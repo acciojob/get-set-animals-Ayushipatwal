c8a8d9a73b47648b0c0e637fda761599fa89d32e
Ayushi Patwal <ayushipatwal03@gmail.com>
2026-03-28 13:42:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
 
-class Cat extends Animal {}
+  get species() {
+    return this._species;
+  }
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
+
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
+
+
+
+const myCat = new Cat("Siamese");
+myCat.makeSound();   
+myCat.purr();        
+
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); 
+myDog.bark();       
